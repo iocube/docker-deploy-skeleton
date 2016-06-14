d48ce97acec38aec5ee98449c6c56bf3fc6bcef4
Vladimir <edoomik@gmail.com>
2016-06-14 22:02:37 +0300

    added database connection initialization and some dummy route

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+import os
+
+import flask
 from flask import Flask
+import pymongo
+
+
+client = pymongo.MongoClient(os.getenv('DATABASE_HOST'))
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'homepage'
+    return 'Flask: {0}'.format(flask.__version__)
+
+
+@app.route('/database')
+def database():
+    build_info = client.db.command('buildinfo')
+    return str(build_info)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
